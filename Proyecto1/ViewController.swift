29c519858b620288a19101d7107277023665f9b7
Juan Alano <gabri.alano@me.com>
2018-07-09 12:52:12 -0300

    Initial Commit

---
+++ Proyecto1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto1
+//
+//  Created by Gabriel Alano on 7/9/18.
+//  Copyright © 2018 Juan Alano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
