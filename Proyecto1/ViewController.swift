09c4606546cf543ad33c9969d9e09396e6dda3ab
Juan Alano <gabri.alano@me.com>
2018-07-10 20:51:28 -0300

    Review author 2

--- Proyecto1/ViewController.swift
+++ Proyecto1/ViewController.swift
@@ -16,18 +16,20 @@ import UIKit
 var regla = ["Par", "Impar", "Bingo", "Viva Swift"]
 var rango = 0...100
 
-
 for num in rango { // Para cada numero dentro del rango, ejecutar las siguientes condiciones:
     
-    if num % 5 == 0 {
-        print (num, regla[2])
-        if num % 2 == 0 {
-            print (num, regla[0])
-        }else{
-            print (num, regla[1])
-        }
+    if num % 2 == 0 {
+        print (num, regla[0])
+    }else{
+        
+        print (num, regla[1])
+        if num % 5 == 0 {
+            print (num, regla[2])
+        } // Pasar para despues de "else" para no confundir, de esta forma imprime todos los numeros del rango consecutivos.
         if num >= 30 && num <= 40 {
             print(num, regla[3])
+            
         }
+        
     }
 }
